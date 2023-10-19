80244477f4df5b13a5fbb98b0763fe5fab52d954
mpeddada1 <mpeddada@google.com>
2023-10-19 15:44:22 +0000

    chore: add sample to run gax-httpjson test concurrently

---
+++ replicate_module.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Replicates the gax-test00 module to gax-test01 to gax-test09.
+
+COUNT=99
+
+for N in `seq ${COUNT}`; do
+  suffix=$(printf "%02d" "${N}")
+  module="gax-test${suffix}"
+  rm -rf "${module}"
+  cp -r gax-test00 $module
+  sed -i.bak -e "s/gax-test00/gax-test${suffix}/" "${module}/pom.xml"
+  sed -i.bak -e "s/gax-test00/gax-test${suffix}/" "${module}/src/test/java/com/google/api/gax/httpjson/HttpJsonDirectServerStreamingCallableTest.java"
+  sed -i.bak -e "s/gax_test00/gax_test${suffix}/" "${module}/src/test/java/com/google/api/gax/httpjson/HttpJsonDirectServerStreamingCallableTest.java"
+  sed -i.bak -e "s/gax-test00/gax-test${suffix}/" "${module}/src/test/resources/logback-test.xml"
+  echo "        <module>${module}</module>"
+done
+
+echo "Ensure to add these modules in the root pom.xml"
